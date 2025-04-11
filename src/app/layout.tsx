11d26402a006908c87f5cc3a08488dff494e32a7
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2025-04-11 10:45:03 +0700

    fix: lint issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,7 +93,7 @@ export default function RootLayout({
             }
           `}
         </Script>
-        <Script>
+        <Script id="gtm-script">
           {`
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
